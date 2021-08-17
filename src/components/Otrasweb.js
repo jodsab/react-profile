8587b849e55f45bae8b6db0e3c8088a0dabf517e
jodsab <85377591+jodsab@users.noreply.github.com>
2021-08-17 01:18:51 -0500

    proyectos actualizados con json

--- src/components/Otrasweb.js
+++ src/components/Otrasweb.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import './Otrasweb.css';
+import players from './Proyectos.json';
+import { FaExternalLinkAlt } from 'react-icons/fa';
 
 function Otrasweb() {
+
+    const arrPlayers = [];
+    players.map((e) => arrPlayers.push(e));
+    
+
     return (
         <div className='webscontainer'>
-            <div>
-                <h4>Esta página web ayuda a mis clientes a calcular el precio de mis servicios como Booster</h4>
-                <iframe height='500' width='360' src='https://jodsab.github.io/using-sass/'></iframe>
-                <p>Visitar Sitio:</p>
-                <a href='https://jodsab.github.io/using-sass/' target='_blank'><button>Jodsab Boosting MMR</button></a>
-            </div>
-            <div>
-                <h4>Este es mi primer perfil de presentación que creé usando  HTML, CSS y Java Script</h4>
-                <iframe height='500' width='360' src='https://jodsab.github.io/cvcamilo/'></iframe>
-                <p>Visitar Sitio:</p>
-                <a href='https://jodsab.github.io/cvcamilo/' target='_blank'><button>Mi primer perfil</button></a>
-            </div>
-            <div>
-                <h4>Red de Dota Pro Players Peruanos</h4>
-                <iframe title="red de pro players peruanos" height='500' width='360' src='https://jodsab.github.io/peru-pro-players/'></iframe>
-                <p>Visitar Sitio:</p>
-                <a href='https://jodsab.github.io/peru-pro-players/' target='_blank' rel="noopener noreferrer"><button>Pro-Players</button></a>
-            </div>
+                {
+                    arrPlayers.map((e) => <div className='title_text_container'>
+                        <div className='proyect_text'>
+                            <h4>{e.name}</h4>
+                            <i>{e.status}</i>
+                            <p> {e.description} </p>
+                        </div>
+                        <iframe src={e.link} width='350' height='500' title={e.name} ></iframe>
+                        <a href={e.link} target="_blank" rel="noreferrer"> <FaExternalLinkAlt /><button>{e.name}</button></a>
+                        </div>)
+                }
         </div>
     )
 }
--- src/components/Redessociales.js
+++ src/components/Redessociales.js
@@ -8,24 +8,24 @@ function Redessociales() {
                 <h4>Redes Sociales</h4>
             </div>
             <div>
-                <img src={require('../img/images/facebook.png').default} />
+                <img src={require('../img/images/facebook.png').default} alt='facebookpng'/>
                 <p>Facebook: </p> 
-                <a href='https://www.facebook.com/jsb.camilo/' target='_blank'>Jodsab Camilo</a>
+                <a href='https://www.facebook.com/jsb.camilo/' target='_blank' rel="noopener noreferrer">Jodsab Camilo</a>
             </div>
             <div>
-                <img src={require('../img/images/instagram.png').default} />
+                <img src={require('../img/images/instagram.png').default} alt='instagrampng'/>
                 <p>Instagram: </p> 
-                <a href='https://www.instagram.com/jodsab.d2/' target='_blank'>@jodsab.d2</a>
+                <a href='https://www.instagram.com/jodsab.d2/' target='_blank' rel="noopener noreferrer">@jodsab.d2</a>
             </div>
             <div>
-                <img src={require('../img/images/linkedin.png').default} />
+                <img src={require('../img/images/linkedin.png').default} alt='linkedinpng'/>
                 <p>Linked In: </p> 
-                <a href='https://www.linkedin.com/in/tito-camilo-bohorquez-quito-1497709b/' target='_blank'>Tito Camilo Bohorquez Quito</a>
+                <a href='https://www.linkedin.com/in/tito-camilo-bohorquez-quito-1497709b/' target='_blank' rel="noopener noreferrer">Tito Camilo Bohorquez Quito</a>
             </div>
             <div>
-                <img src={require('../img/images/github.png').default} />
+                <img src={require('../img/images/github.png').default} alt='githubpng'/>
                 <p>Git Hub: </p> 
-                <a href='https://github.com/jodsab' target='_blank'>Jodsab</a>
+                <a href='https://github.com/jodsab' target='_blank' rel="noopener noreferrer">Jodsab</a>
             </div>
         </div>
     )
