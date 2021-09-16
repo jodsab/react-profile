de0e360e173f7a79d53324350995149ad51d1975
jodsab <85377591+jodsab@users.noreply.github.com>
2021-09-16 12:21:28 -0500

    no recuerdo que es

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Proyectos from './pages/Proyectos';
 import Conocimientos from './pages/Conocimientos';
 import Contacto from './pages/Contacto';
 import Error from './pages/Error';
 
+
+
 export function App() {
+
   return (
     <>
       <Router>
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
-          <Route path='/conocimientos'  component={Conocimientos} />
-          <Route path='/proyectos'  component={Proyectos} />
-          <Route path='/contacto'  component={Contacto} />
+          <Route path='/conocimientos' component={Conocimientos} />
+          <Route path='/proyectos' component={Proyectos} />
+          <Route path='/contacto' component={Contacto} />
           <Route path='/' component={Error} />
         </Switch>
       </Router>
