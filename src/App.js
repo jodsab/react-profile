8c766daf57936f5229cf9b0885b0c4e8fa8be154
jodsab <85377591+jodsab@users.noreply.github.com>
2021-09-23 05:06:04 -0500

    hashrouter added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Switch, Route, HashRouter } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Proyectos from './pages/Proyectos';
@@ -15,8 +15,8 @@ export function App() {
 
   return (
     <>
-      <Router>
-        <Navbar />
+      <HashRouter>
+      <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/conocimientos' component={Conocimientos} />
@@ -24,8 +24,7 @@ export function App() {
           <Route path='/contacto' component={Contacto} />
           <Route path='/' component={Error} />
         </Switch>
-      </Router>
-
+      </HashRouter>
     </>
   );
 }
